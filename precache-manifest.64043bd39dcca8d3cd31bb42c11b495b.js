79d3dba7fe582c05926e21f65e35a4ebd45f7d4b
Top Deuse <topdeuse@gmail.com>
2019-02-18 18:39:22 +0300

    Updates

---
+++ precache-manifest.64043bd39dcca8d3cd31bb42c11b495b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c488b8995fb3104bc0a72444ef334ed7",
+    "url": "/smart-advertisment/static/media/combined-shape-copy-9.c488b899.svg"
+  },
+  {
+    "revision": "205689f2127f7ef7961410d538d72a14",
+    "url": "/smart-advertisment/static/media/Kappa.205689f2.jpg"
+  },
+  {
+    "revision": "0038580f398e18a55662",
+    "url": "/smart-advertisment/static/js/runtime~main.0038580f.js"
+  },
+  {
+    "revision": "d3eff3732efcf04510af",
+    "url": "/smart-advertisment/static/js/main.d3eff373.chunk.js"
+  },
+  {
+    "revision": "75f07f6bda4332137859",
+    "url": "/smart-advertisment/static/js/2.75f07f6b.chunk.js"
+  },
+  {
+    "revision": "d3eff3732efcf04510af",
+    "url": "/smart-advertisment/static/css/main.9b530aa1.chunk.css"
+  },
+  {
+    "revision": "6bff5e43312eca77962f4c0ba5b1ea69",
+    "url": "/smart-advertisment/index.html"
+  }
+];
